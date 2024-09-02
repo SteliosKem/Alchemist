582e4a363402d627fe647ea2dc04817c6b5d141f
SteliosKem <stylianoskem@gmail.com>
2024-09-02 18:57:23 +0300

    Update PointMass.h

--- Alchemist/src/PointMass.h
+++ Alchemist/src/PointMass.h
@@ -5,6 +5,22 @@
 namespace Alchemist {
 	class PointMass2D {
 	public:
+		PointMass2D(float mass_inverse, float damping = 1, const glm::vec2& acceleration = {0.0f, 0.0f}, 
+			const glm::vec2& velocity = { 0.0f, 0.0f }, const glm::vec2& position = { 0.0f, 0.0f })
+			: m_acceleration{ acceleration }, m_velocity{ velocity }, m_position{ position }
+			, m_mass_inverse{ mass_inverse }, m_damping{damping} {}
+
+		void set_acceleration(const glm::vec2& a) { m_acceleration = a; }
+		void set_velocity(const glm::vec2& a) { m_velocity = a; }
+		void set_position(const glm::vec2& a) { m_position = a; }
+		void set_mass(float mass) { m_mass_inverse = 1 / mass; }
+		void set_damp(float damp) { m_damping = damp; }
+
+		glm::vec2 get_position() const { return m_position; }
+		glm::vec2 get_velocity() const { return m_velocity; }
+		glm::vec2 get_acceleration() const { return m_acceleration; }
+		float get_mass() const { return 1 / m_mass_inverse; }
+		float get_damping() const { return m_damping; }
 	private:
 		void integrate(Timestep dt);
 	private:

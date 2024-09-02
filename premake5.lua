127d7968a4c4a7a959af8d81444040824ae74826
SteliosKem <stylianoskem@gmail.com>
2024-09-02 14:43:45 +0300

    Project Setup

---
+++ premake5.lua
@@ -0,0 +1,52 @@
+workspace "Alchemist"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["glm"] = "Ivory/vendor/glm"
+
+project "Alchemist"
+	location "Alchemist"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "Alchemist/src/pch.cpp"
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
+	}
+
+	includedirs {
+		"%{prj.name}/src",
+		"%{IncludeDir.glm}",
+	}
+	systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
